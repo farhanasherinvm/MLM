e1ebba68a4bbfc3b4704f5a962b74572a5bf77fd
Abhijith S <aabhijith2357@gmail.com>
2025-10-02 14:41:35 +0530

    Port 25

--- backend/settings.py
+++ backend/settings.py
@@ -176,7 +176,7 @@ DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1")
 if not DEBUG:  # ✅ Production (Render, Gmail SMTP)
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_PORT = 465
+    EMAIL_PORT = 25
     EMAIL_USE_TLS = True
     EMAIL_USE_SSL = False
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "zecserbusiness@gmail.com")
